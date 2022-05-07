730dc730858dac18a1b285b25b63b8afed9eaea1
Mira <5555159m@gmail.com>
2022-05-08 01:29:18 +0300

    added 1 task

---
+++ 01_Square/Program.cs
@@ -0,0 +1,12 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("Да, число a = b^2 :");
+}
+else {
+    Console.WriteLine("Нет, число a != b^2 :");
+}
