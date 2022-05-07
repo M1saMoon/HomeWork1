55d20dee6e0cc8541addc9d85f49b96700ad7202
Mira <5555159m@gmail.com>
2022-05-08 02:08:08 +0300

    added 9 task

--- 09_ThirdDigit/Program.cs
+++ 09_ThirdDigit/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти третью цифру числа или сообщить, что её нет
+
+int num = new Random().Next();
+//int num = 25;
+Console.WriteLine(num);
+if (num < 100) Console.WriteLine("У введенного числа нет 3 цифры!");
+else {
+    // 123 = 123 % 10
+    // 1234 = 1234 / 10 % 10 
+    // 12345 = 12345 / 10 
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.WriteLine("Третья цифра равна =" +result);
+}
