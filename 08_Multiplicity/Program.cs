1027904af88bc3ce8b40edc010841b0be5e9b8f6
Mira <5555159m@gmail.com>
2022-05-08 02:05:26 +0300

    added 8 task

--- 08_Multiplicity/Program.cs
+++ 08_Multiplicity/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите делимое:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите делитель:");
+int b = int.Parse(Console.ReadLine());
+if (a % b == 0) Console.WriteLine($"Число {a} кратно числу {b}");
+else Console.WriteLine ($"При делении {a} на {b} получаем остаток {a % b}");
