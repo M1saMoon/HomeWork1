1bfd6b7609ad7ac8fa15b534d7bba02024037b36
Mira <5555159m@gmail.com>
2022-05-08 01:51:42 +0300

    added 4 task

--- 04_RangeNum/Program.cs
+++ 04_RangeNum/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Показать четные числа от 1 до N
+
+Console.Write("Введите диапазон n:");
+int n = int.Parse(Console.ReadLine());
+int count = 2;
+Console.WriteLine($"Четные числа от 1 до {n}:");
+while (count <= n)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
