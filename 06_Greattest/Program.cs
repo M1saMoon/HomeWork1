35f4d9066b038f8df3ba3a6a9b5c6fefdd8452df
Mira <5555159m@gmail.com>
2022-05-08 01:59:03 +0300

    added 6 task

--- 06_Greattest/Program.cs
+++ 06_Greattest/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+//Console.Write("Введите двухзначное число: ");
+//int n = int.Parse(Console.ReadLine());
+
+int n = new Random().Next(10,100);
+Console.WriteLine(n);
+if (n > 9 && n < 100)
+{
+    int a = n / 10;
+    int b = n % 10;
+    if (a > b) Console.WriteLine($"Наибольшая цифра {a}");
+    else Console.WriteLine("В числе "+ n + " Наибольшая цифра: " + b);
+}
+else Console.WriteLine("Ввели не правильное число");
