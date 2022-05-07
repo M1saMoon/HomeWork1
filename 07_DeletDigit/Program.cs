ae595f6f1110ebff9247f5602d288191fe49cf0a
Mira <5555159m@gmail.com>
2022-05-08 02:01:59 +0300

    added 7 task

--- 07_DeletDigit/Program.cs
+++ 07_DeletDigit/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите 3х значное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    //654 = a b c = a * 100 + b * 10 + c 
+    //64 = a c = a * 10 + c
+
+Console.WriteLine((n / 100) * 10 + n % 10);
+}
+else Console.WriteLine("Введено не 3х значное число!");
